44ac33b692baf4b5cc4298381eb57eb7325b9aeb
Lasse Egebo <lae003@edu.zealand.dk>
2025-05-20 12:27:38 +0200

    ShowPastDayTest

--- Selenium_3fSemProj2025_SYM/SYMProj.cs
+++ Selenium_3fSemProj2025_SYM/SYMProj.cs
@@ -8,16 +8,16 @@ namespace Selenium_3fSemProj2025_SYM
     public sealed class SYMProj
     {
 
-        //private static readonly string DriverDirector = "C:\\webdrivers";
-        private static readonly string DriverDirector = "C:\\WebDriver";
+        private static readonly string DriverDirector = "C:\\webdrivers";
+        //private static readonly string DriverDirector = "C:\\WebDriver";
         private static IWebDriver _driver;
 
 
         [ClassInitialize]
         public static void setup(TestContext context)
         {
-            //_driver = new FirefoxDriver(DriverDirector);
-            _driver = new ChromeDriver(DriverDirector);
+            _driver = new FirefoxDriver(DriverDirector);
+            //_driver = new ChromeDriver(DriverDirector);
         }
 
         //[ClassCleanup]
@@ -29,19 +29,25 @@ namespace Selenium_3fSemProj2025_SYM
         [TestInitialize]
         public void InitializeTest()
         {
-            //string overview = "C:\\Datamatiker_3_sem\\3fSemesterProjekt2025\\oversigt.html";
-            string overview = "C:\\Users\\jonas\\3SemProjFront\\3fSemesterProjektHjemmeside\\oversigt.html";
-            _driver.Navigate().GoToUrl(overview);
+            string index = "C:\\Datamatiker_3_sem\\3fSemesterProjekt2025\\index.html";
+            //string overview = "C:\\Users\\jonas\\3SemProjFront\\3fSemesterProjektHjemmeside\\oversigt.html";
+            _driver.Navigate().GoToUrl(index);
 
             Assert.AreEqual("3fSemesterProjekt", _driver.Title);
-             
+
+            IWebElement overviewButton = _driver.FindElement(By.Id("earlier_incidents_button"));
+            overviewButton.Click();
         }
 
+
         [TestMethod]
-        public void TestMethod1()
+        public void ShowPastDayTest()
         {
-            IWebElement mainPage = _driver.FindElement(By.Id("main_page_button"));
-            mainPage.Click();
+            IWebElement periodButton = _driver.FindElement(By.Id("choose_period_button"));
+            periodButton.Click();
+
+            IWebElement pastDayButton = _driver.FindElement(By.Id("past_day_option"));
+            pastDayButton.Click();
         }
     }
 }
