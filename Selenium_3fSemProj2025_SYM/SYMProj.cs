092f1eb4eea3be94dcdd73d4286a288072d16f99
Lasse Egebo <lae003@edu.zealand.dk>
2025-05-20 10:35:46 +0200

    Add project files.

---
+++ Selenium_3fSemProj2025_SYM/SYMProj.cs
@@ -0,0 +1,11 @@
+﻿namespace Selenium_3fSemProj2025_SYM
+{
+    [TestClass]
+    public sealed class SYMProj
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
