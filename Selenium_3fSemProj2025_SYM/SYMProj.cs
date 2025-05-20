d69f551a949013f7d6fede24d9909b0241a692b8
Lasse Egebo <lae003@edu.zealand.dk>
2025-05-20 10:43:32 +0200

    Add Selenium setup and teardown methods for testing
    - Updated namespace to include Selenium libraries.
    - Introduced `DriverDirector` for web driver path.
    - Added `_driver` variable to manage the web driver instance.
    - Implemented `Setup` method for initializing `FirefoxDriver`.
    - Created `TearDown` method to dispose of the web driver.
    - Added `InitializeTest` method for test environment setup.
    - Maintained existing `TestMethod1` within the new structure.

--- Selenium_3fSemProj2025_SYM/SYMProj.cs
+++ Selenium_3fSemProj2025_SYM/SYMProj.cs
@@ -1,8 +1,38 @@
-﻿namespace Selenium_3fSemProj2025_SYM
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+
+namespace Selenium_3fSemProj2025_SYM
 {
     [TestClass]
     public sealed class SYMProj
     {
+
+        private static readonly string DriverDirector = "C\\webdrivers";
+
+        private static IWebDriver _driver;
+
+
+        [ClassInitialize]
+        public static void Setup(TestContext context)
+        {
+            _driver = new FirefoxDriver(DriverDirector);
+            //_driver = new ChromeDriver(DriverDirector);
+        }
+
+        [ClassCleanup]
+        public static void TearDown()
+        {
+            _driver.Dispose();
+        }
+
+        [TestInitialize]
+        public void InitializeTest()
+        {
+            string mainPage = "C:\\Datamatiker_3_sem\\3fSemesterProjekt2025\\index.html";
+            string 
+        }
+
         [TestMethod]
         public void TestMethod1()
         {
